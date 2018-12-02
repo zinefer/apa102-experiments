1e62a8d27833a86372341161dd9276ba86259e91
zinefer <zinefer@gmail.com>
2018-12-01 23:23:45 -0700

    Rainbow pattern

--- rainbow.cpp
+++ rainbow.cpp
@@ -4,11 +4,14 @@
 #include <stdint.h>
 #include <unistd.h>
 
+#include <timer.cpp>
+#include <colors.cpp>
+
 int to_int(char const *s);
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " NUM_LEDS -b BRIGHTNESS -d HUE_DELTA -h INITIAL_HUE -i HUE_INTERVAL" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " NUM_LEDS -b BRIGHTNESS -d HUE_DELTA -h INITIAL_HUE -i HUE_INTERVAL -g CHANCE_OF_GLITTER" << std::endl;
     return 1;
   }
 
@@ -18,13 +21,13 @@ int main(int argc, char* argv[]) {
   uint8_t HUE_DELTA = 15;
   uint8_t INITIAL_HUE = 1;
 
-  int HUE_INTERVAL = 250;
+  int HUE_INTERVAL = 20;
+
+  bool CHANCE_OF_GLITTER = 0;
 
   for (int i = 2; i < argc; ++i) {
 
     std::string arg = std::string(argv[i]);
-
-    std::cout << arg << ":" << i << std::endl;
 
     if (arg == "-b") {
       BRIGHTNESS = to_int(argv[++i]);
@@ -42,6 +45,10 @@ int main(int argc, char* argv[]) {
       HUE_INTERVAL = to_int(argv[++i]);
     }
 
+    if (arg == "-g") {
+      CHANCE_OF_GLITTER = to_int(argv[++i]);
+    }
+
   }
 
   wiringPiSetup();
@@ -49,12 +56,53 @@ int main(int argc, char* argv[]) {
     std::cerr << "wiringPiSPISetup failed" << std::endl;
   }
 
+  RgbColor white = { 255, 255, 255 };
 
-  std::cout << "NUM_LEDS: " << NUM_LEDS << std::endl;
-  std::cout << "BRIGHTNESS: " << (int)BRIGHTNESS << std::endl;
-  std::cout << "HUE_DELTA: " << (int)HUE_DELTA << std::endl;
-  std::cout << "INITIAL_HUE: " << (int)INITIAL_HUE << std::endl;
-  std::cout << "HUE_INTERVAL: " << (int)HUE_INTERVAL << std::endl;
+  Timer timer;
+
+  while(true)
+  {
+
+    if (timer.elapsed() >= HUE_INTERVAL) {
+      timer.reset();
+
+      INITIAL_HUE = (INITIAL_HUE + 1) % 255;
+    }
+
+    uint8_t buf[1];
+    for(int i = 0; i < 4; i++) {
+      buf[0] = 0x00;
+      wiringPiSPIDataRW(0, buf, 1);
+    }
+
+    HsvColor color = { INITIAL_HUE, 255, 240 };
+
+    for(int i = 0; i < NUM_LEDS; i++) {
+      RgbColor frame_color = HsvToRgb(color);
+
+      if (CHANCE_OF_GLITTER > 0) {
+        if ((rand() % 100) < CHANCE_OF_GLITTER) {
+          frame_color = white;
+        }
+      }
+
+      uint8_t frame[4];
+      frame[0] = 0b11100000 | (0b00011111 & BRIGHTNESS);
+      frame[1] = frame_color.b;
+      frame[2] = frame_color.g;
+      frame[3] = frame_color.r;
+
+      wiringPiSPIDataRW(0, frame, 4);
+
+      color.h = (color.h + HUE_DELTA) % 255;
+    }
+
+    for(int i = 0; i < NUM_LEDS; i++) {
+      buf[0] = 0xFF;
+      wiringPiSPIDataRW(0, buf, 1);
+    }
+
+  }
 
 }
 
